0c95bc8a51434a7e8e64d43d17499ee76b0757c3
LouisZhong <destinyxll@gmail.com>
2023-05-13 16:55:02 +0800

    extract components and import data

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,10 @@
-// import minus from 'icons/minus.svg'
-// import plus from 'icons/plus.svg'
 import Product from './components/Cart/Cart';
 import './App.css';
 
-
 function Cart() {
-  return (
-    <>      
-      
-        <Product />
-
-
-    </>
+  return (         
+    <Product />
   )
 }
 
 export default Cart;
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload..
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
-
-// export default App;
--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,53 +1,62 @@
 
-// import minus from 'assets/icons/minus.svg'
-import minus from '../../assets/icons/minus.svg'
-import plus from '../../assets/icons/plus.svg'
-import cartScss from './Cart.module.scss'
-import product1 from '../../assets/images/product-1.jpg'
-import product2 from '../../assets/images/product-2.jpg'
+import minus from 'assets/icons/minus.svg'
+import plus from 'assets/icons/plus.svg'
+import cartScss from 'components/Cart/Cart.module.scss'
 
+const items = [
+  {
+    id: '1',
+    name: '貓咪罐罐',
+    img: 'https://picsum.photos/300/300?text=1',
+    price: 100,
+    quantity: 2,
+  },
+  {
+    id: '2',
+    name: '貓咪干干',
+    img: 'https://picsum.photos/300/300?text=2',
+    price: 200,
+    quantity: 1,
+  },
+]
+
+function ItemRender () {
+  const listItems = items.map(item => 
+    <div key={item.id} className={cartScss.productContainer} data-count="0" data-price="3999">
+      <img src={item.img} className={cartScss.productImg} />
+      <div className={cartScss.productInfo}>
+        <div className={cartScss.productName}>{item.name}</div>
+        <div className={cartScss.productControlContainer}>
+          <div className={cartScss.productControl}>
+            <img src={minus} className={cartScss.minus} alt="minus"/>
+            <span className={cartScss.productCount}>1</span>
+            <img src={plus} className={cartScss.plus} alt="plus" />
+          </div>
+        </div>
+        <div className={cartScss.productPrice}>{item.price}</div>
+      </div>
+    </div>
+  );
+
+  return (
+    <>{listItems}</>
+  )  
+}
 
 export default function Product () {
   return (
-    <section className={cartScss.cartContainer}>
+    <section className={cartScss.cartContainer}>      
       <h3 className={cartScss.productTitle}>購物籃</h3>
       <section className={cartScss.productList} data-total-price="0">
-        <div className={cartScss.productContainer} data-count="0" data-price="3999">
-          <img src={product1} className={cartScss.productImg} />
-          <div className={cartScss.productInfo}>
-            <div className={cartScss.productName}>破壞補丁修身牛仔褲</div>
-            <div className={cartScss.productControlContainer}>
-              <div className={cartScss.productControl}>
-                <img src={minus} className={cartScss.minus} alt="minus"/>
-                <span className={cartScss.productCount}>1</span>
-                <img src={plus} className={cartScss.plus} alt="plus" />
-              </div>
-            </div>
-            <div className={cartScss.productPrice}>3999</div>
-          </div>
-        </div>
-        <div className={cartScss.productContainer} data-count="0" data-price="1299">
-          <img src={product2} className={cartScss.productImg} />
-          <div className={cartScss.productInfo}>
-            <div className={cartScss.productName}>刷色直筒牛仔褲</div>
-            <div className={cartScss.productControlContainer}>
-              <div className={cartScss.productControl}>
-                <img src={minus} className={cartScss.minus} alt="minus" />
-                <span className={cartScss.productCount}>1</span>
-                <img src={plus} className={cartScss.plus} alt="plus" />
-              </div>
-            </div>
-            <div className={cartScss.productPrice}>1299</div>
-          </div>
-        </div>
+        <ItemRender />
       </section>
       <section className={`${cartScss.cartInfo} ${cartScss.shipping}`}>
         <div class="text">運費</div>
-        <div class="price">free</div>
+        <div className={cartScss.price}>free</div>
       </section>
       <section className={`${cartScss.cartInfo} ${cartScss.total}`}>
         <div class="text">小計</div>
-        <div class="price">5200</div>
+        <div className={cartScss.price}>5200</div>
       </section>
     </section>
   )
